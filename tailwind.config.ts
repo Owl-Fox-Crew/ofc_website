7d12f4e35b6e1f73a5bfaf95edb08f610ef88782
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-07-26 20:50:54 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,26 @@
-import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import type { Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
   darkMode: ['class'],
-  content: ['./src/app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  content: [
+    './src/app/**/*.{ts,tsx}',
+    './src/components/**/*.{ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
-        brand: { primary: '#FCD757', black: '#000', white: '#fff' }
+        brand: {
+          primary: '#FCD757',
+          black: '#000000',
+          white: '#FFFFFF',
+        }
       },
-      fontFamily: { sans: ["Inter", ...fontFamily.sans] },
-      transitionTimingFunction: { cinematic: 'ease-in-out' }
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
+      }
     }
   },
   plugins: [require('tailwindcss-animate')]
-};
-
-export default config;
+}
+export default config
