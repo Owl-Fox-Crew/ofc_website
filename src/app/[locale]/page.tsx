75283c0372f0c3c0e8496ce98412a127b23ed251
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-03 01:02:03 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import Hero from '../../components/home/Hero';
+import Hero from '@/components/home/Hero';
 import { useEffect } from 'react';
 import { trackEvent } from '@/lib/analytics';
-import { Benefits } from '../../components/home/Benefits';
-import { EaseForYou } from '../../components/home/EaseForYou';
-import { Cases } from '../../components/home/Cases';
+import { Benefits } from '@/components/home/Benefits';
+import { EaseForYou } from '@/components/home/EaseForYou';
+import { Cases } from '@/components/home/Cases';
 
 export default function Home() {
   useEffect(() => {
