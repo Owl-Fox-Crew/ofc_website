79c38b5ffb0ac659ac04a0b46866211f063c793f
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-03 00:28:03 +0200

    Create page.tsx

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+
+export default function LocaleLayout({
+  children,
+  params: { locale }
+}: {
+  children: ReactNode;
+  params: { locale: string };
+}) {
+  const messages = useMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
