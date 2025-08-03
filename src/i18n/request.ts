25e70771ef504127a2ebe1875f0ab6e9c2de61e3
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-03 02:56:35 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,7 +1,9 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(({ locale }) => ({
-  locale: locale ?? 'en',
-  messages: (async () =>
-    (await import(`../messages/${locale ?? 'en'}/Hero.json`)).default)(),
-}));
+export default getRequestConfig(async ({ locale }) => {
+  const messages = await import(`../messages/${locale}/Hero.json`);
+  return {
+    locale,
+    messages: messages.default.Hero
+  };
+});
