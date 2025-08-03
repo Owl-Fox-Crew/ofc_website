16c6ef6c00732d2560df8d6360e0413c8b68a258
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-03 19:43:51 +0200

    Update request.ts

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,8 +3,21 @@ import { getRequestConfig } from 'next-intl/server';
 export default getRequestConfig(({ locale }) => {
   const safeLocale = locale ?? 'en';
 
+  let messages;
+  switch (safeLocale) {
+    case 'es':
+      messages = require('../messages/es/Hero.json');
+      break;
+    case 'en':
+      messages = require('../messages/en/Hero.json');
+      break;
+    default:
+      messages = require('../messages/en/Hero.json');
+      break;
+  }
+
   return {
     locale: safeLocale,
-    messages: () => import(`../messages/${safeLocale}/Hero.json`).then(mod => mod.default)
+    messages,
   };
 });
