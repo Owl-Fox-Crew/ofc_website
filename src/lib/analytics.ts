b2d50551fc40bb1b6aab25b493094d321ab75e72
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-06 04:21:00 +0200

    Delete analytics.ts

--- src/lib/analytics.ts
+++
@@ -1,9 +0,0 @@
-export const trackEvent = (name: string, params: Record<string, any> = {}) => {
-  if (typeof window === 'undefined') return
-  // GA4
-  // @ts-ignore
-  if (window.gtag) {
-    // @ts-ignore
-    window.gtag('event', name, params)
-  }
-}
