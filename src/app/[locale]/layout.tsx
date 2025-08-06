9f1352ed293db1e067815558c02e0feda306adcb
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-08-06 04:20:01 +0200

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,30 +1,6 @@
 // src/app/[locale]/layout.tsx
 import { ReactNode } from 'react';
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-import ClientHeader from '@/components/common/ClientHeader'; // Usamos el wrapper cliente
 
-export function generateStaticParams() {
-  return [{ locale: 'es' }, { locale: 'en' }];
-}
-
-export default async function LocaleLayout({
-  children,
-  params: { locale }
-}: {
-  children: ReactNode;
-  params: { locale: string };
-}) {
-  const messages = await getMessages({ locale });
-
-  return (
-    <html lang={locale}>
-      <body>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <ClientHeader />
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+export default function LocaleLayout({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
