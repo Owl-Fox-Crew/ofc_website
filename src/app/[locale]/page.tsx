8afaa04436d2786f70f27c8ad577147fe12d72b0
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-09-28 05:06:33 +0200

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,23 @@
-'use client';
-
 import Hero from '@/components/home/Hero';
+import BrandValues from '@/components/home/BrandValues';
+import ValueOffer from '@/components/home/ValueOffer';
+import CommunicationKeys from '@/components/home/CommunicationKeys';
+import Workflow from '@/components/home/Workflow';
+import BrandStories from '@/components/home/BrandStories';
+import FAQs from '@/components/home/FAQs';
+import FAQForm from '@/components/home/FAQForm';
 
-export default function Page() {
-  return <Hero />;
+export default function HomePage() {
+  return (
+    <main>
+      <Hero />
+      <BrandValues />
+      <ValueOffer />
+      <CommunicationKeys />
+      <Workflow />
+      <BrandStories />
+      <FAQs />
+      <FAQForm />
+    </main>
+  );
 }
