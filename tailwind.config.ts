9342ad7f0ef53da37ee8fbef2ba472d80727b585
Owl-Fox-Crew <owlfoxcrew@gmail.com>
2025-09-28 05:09:48 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,13 +14,18 @@ const config: Config = {
           primary: '#FCD757',
           black: '#000000',
           white: '#FFFFFF',
+          orange: '#FF7503',
+          blue: '#5798FF',
+          purple: '#4D2682',
+          darkgray: '#231F20'
         }
       },
       fontFamily: {
         sans: ["Inter", ...fontFamily.sans],
+        raleway: ["Raleway", "sans-serif"],
       }
     }
   },
   plugins: [require('tailwindcss-animate')]
 }
-export default config
+export default config;
